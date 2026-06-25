f27e915cdff31ad574e69a65223f4afa2159dc25
Ammar Abo Handoka <ammar01032815787@gmail.com>
2026-06-25 18:41:16 +0300

    First commit

---
+++ project1.py
@@ -0,0 +1,4 @@
+print ("Welcome to the Youtube Channel Name Generator:")
+nick_name = input ("What is your nickname?\n")
+Topic = input ("What is your Channel about?\n")
+print ("You could name your Channel :( "+Topic+" with "+nick_name+")")
